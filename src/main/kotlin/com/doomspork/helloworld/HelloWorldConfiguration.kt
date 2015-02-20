e76ad306d3a4a0f1852f9d4bc60a84a0c96b6560
Sean Callan <seancallan@gmail.com>
2015-02-19 22:27:17 -0800

    Update jackson annotations to support multi-arg constructor

--- src/main/kotlin/com/doomspork/helloworld/HelloWorldConfiguration.kt
+++ src/main/kotlin/com/doomspork/helloworld/HelloWorldConfiguration.kt
@@ -3,6 +3,7 @@ package com.doomspork.helloworld
 import io.dropwizard.Configuration
 import org.hibernate.validator.constraints.NotEmpty
 import com.fasterxml.jackson.annotation.JsonProperty
+import com.fasterxml.jackson.annotation.JsonCreator
 
-data class HelloWorldConfiguration(JsonProperty NotEmpty var template: String,
-                                   JsonProperty NotEmpty var defaultName: String = "Stranger") : Configuration()
+data class HelloWorldConfiguration [JsonCreator] (JsonProperty("template") NotEmpty var template: String = "",
+                                   JsonProperty("defaultName") NotEmpty var defaultName: String = "Stranger") : Configuration()
