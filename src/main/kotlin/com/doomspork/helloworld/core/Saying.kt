3f6826c09a38e631f8ca879151797c23e5a7487f
Chen Xu <windoze@0d0a.com>
2015-11-09 23:10:32 +0800

    Use new annotation syntax

--- src/main/kotlin/com/doomspork/helloworld/core/Saying.kt
+++ src/main/kotlin/com/doomspork/helloworld/core/Saying.kt
@@ -3,4 +3,4 @@ package com.doomspork.helloworld.core
 import com.fasterxml.jackson.annotation.JsonProperty
 import org.hibernate.validator.constraints.Length
 
-data class Saying(JsonProperty val id: Long, JsonProperty Length(max = 3) val content: String)
+data class Saying(@JsonProperty val id: Long, @JsonProperty @Length(max = 3) val content: String)
